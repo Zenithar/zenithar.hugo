d69439f429c5fc114f166749a4ddd4811ee2a009
Thibault Normand <me@zenithar.org>
2023-03-02 10:21:21 +0100

    chore(ci): update flake files
    Signed-off-by: Thibault Normand <me@zenithar.org>

--- flake.nix
+++ flake.nix
@@ -5,14 +5,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-parts.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       # modify these as needed if you're using a different system
       systems = [ "x86_64-linux" "aarch64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
+      perSystem = { config, inputs', pkgs, system, ... }: {
         devShells.default = import ./shell.nix { inherit pkgs; };
         packages.default = pkgs.callPackage ./site.nix {};
       };
--- site.nix
+++ site.nix
@@ -1,16 +1,13 @@
-{ stdenv, hugo, scour }:
+{ stdenv, hugo }:
 stdenv.mkDerivation {
   name = "static-site";
   src = ./.;
   nativeBuildInputs = [
     # specify site build dependencies here
     hugo
-    # optimize SVGs
-    scour
   ];
   buildPhase = ''
     # prepare and build the site
-    scour -i favicon-original.svg -o favicon.svg
     hugo -D
   '';
   installPhase = ''
